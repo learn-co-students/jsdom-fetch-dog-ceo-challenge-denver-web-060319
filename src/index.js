6ae047b340c890d8100f8838d6c9ce3d95519a95
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-07-21 16:18:12 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random"
+   
+    
+    fetch(imgUrl)
+    .then(parseJson)
+    .then(getDogCEOImg)
+    
+    function parseJson(resp)
+    {
+        return resp.json()
+    }
+
+    function getDogCEOImg(dogs)
+    {
+        const dogImageContainer = document.getElementById("dog-image-container")
+        const img = document.createElement("img")
+        img.src = `${dogs.message}`
+        dogImageContainer.appendChild(img)
+    }
+
+    
+})
