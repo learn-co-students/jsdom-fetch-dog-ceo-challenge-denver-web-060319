a29184af283c20b6a0b20c842501d4de8964d4d8
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-07-21 22:24:26 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgUrl = "https://dog.ceo/api/breeds/image/random"
+    fetchDogImg()
+    fetchDogBreeds()
+
+    function fetchDogImg()
+    {
+        const imgUrl = "https://dog.ceo/api/breeds/image/random"
    
-    
-    fetch(imgUrl)
-    .then(parseJson)
-    .then(getDogCEOImg)
-    
+        return fetch(imgUrl)
+        .then(parseJson)
+        .then(getDogCeoImg)
+    }
+
+    function fetchDogBreeds()
+    {
+        const breedUrl = "https://dog.ceo/api/breeds/list/all"
+
+        return fetch(breedUrl)
+        .then(parseJson)
+        .then(getDogBreeds)
+    }
+
+       
     function parseJson(resp)
     {
         return resp.json()
     }
 
-    function getDogCEOImg(dogs)
+    function getDogCeoImg(dogs)
     {
         const dogImageContainer = document.getElementById("dog-image-container")
         const img = document.createElement("img")
@@ -19,5 +34,26 @@ document.addEventListener('DOMContentLoaded', () => {
         dogImageContainer.appendChild(img)
     }
 
+    function getDogBreeds(dogs)
+    {
+        const dogBreeds = document.getElementById("dog-breeds")
+      
+        for(breed in dogs["message"])
+        {
+            const li = document.createElement("li")
+            li.innerText = breed
+            changeBreedColor(li)
+            dogBreeds.appendChild(li)
+        }     
+    }
+
+    function changeBreedColor(li)
+    {
+        li.addEventListener("click", () => {
+            li.style.color = "green"
+        })  
+    }
+  
+  
     
 })
